d0efe595bec692c0e435a73187e4bdd9206a59f4
Fl0ti <fl0timail25@gmail.com>
2023-08-07 02:22:59 +0200

    2

--- Device-shop/scripts/sticky-navbar.js
+++ Device-shop/scripts/sticky-navbar.js
@@ -9,7 +9,6 @@ let sticky = navbar.offsetTop;
 function myFunction() {
     if(window.scrollY >= sticky) {
         navbar.classList.add("sticky")
-        // navbar.classList.add("background: gray")
     } else {
         navbar.classList.remove("sticky");
     }
